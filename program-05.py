6b58780bef4fcf6272b4cb7e4c8758ec7ab8e7b3
walidayankawsar <walidayankawsar@gmail.com>
2025-08-15 07:28:09 -0700

    print random number

---
+++ program-05.py
@@ -0,0 +1,19 @@
+# Write a python program to generate a random number.
+
+import random
+print(f"Random number: {random.randint(1, 100)}")
+
+
+# or
+ 
+import random
+
+number = random.randint(1, 10)
+print(number)
+
+
+# random = মডিউল/Library
+
+# randint = সেই Library ভেতরের ফাংশন
+
+# (1, 100) = ফাংশনের প্যারামিটার
