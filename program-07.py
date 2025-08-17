6ababb7093b1bc370252f8463f15b5fdb7c82ad1
walidayankawsar <walidayankawsar@gmail.com>
2025-08-17 05:50:38 -0700

    convert celsius to fahrenheit

---
+++ program-07.py
@@ -0,0 +1,8 @@
+# write a python program to convert celsius to fahrenheit.
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+#conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"convert celsius: {celsius} to Fahrenheit: {fahrenheit}")
